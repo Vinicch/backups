f7a613f8d95b479fffd611bc56862ed97a5220a6
Vinicius Leme Alves <52262918+Vinicch@users.noreply.github.com>
2022-02-03 20:31:20 -0300

    Add files via upload

---
+++ post-install.sh
@@ -0,0 +1,32 @@
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${YELLOW}Updating mirrors...${NC}"
+
+# If using Manjaro
+# sudo pacman-mirrors -f 5
+
+# If using other Arch distros
+sudo pacman -S reflector
+sudo reflector -f 30 -l 30 --number 10 -p http -p https --verbose --save /etc/pacman.d/mirrorlist
+
+
+echo -e "${YELLOW}Installing base dev tools...${NC}"
+
+sudo pacman -S base-devel bash-completion bashtop docker docker-compose vim git wget tar unrar zip unzip
+
+echo -e "${YELLOW}Installing Pacman helper...${NC}"
+
+cd $HOME && git clone https://aur.archlinux.org/paru-bin.git
+cd $HOME/paru-bin && makepkg -si
+cd $HOME && rm -rf paru-bin
+
+echo -e "${YELLOW}Updating system...${NC}"
+
+paru
+
+echo -e "${YELLOW}Setting up Docker...${NC}"
+
+sudo usermod -aG docker $USER
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
