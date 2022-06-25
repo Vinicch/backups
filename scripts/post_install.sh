e8d30bbdb06f36b65b83ab1eb13ef05d879c252a
Vinicius Leme Alves <52262918+Vinicch@users.noreply.github.com>
2022-06-25 02:42:58 -0300

    Update post_install.sh

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -4,12 +4,11 @@ NC='\033[0m'
 echo -e "${YELLOW}Updating mirrors...${NC}"
 
 # If using Manjaro
-# sudo pacman-mirrors -f 5
+# sudo pacman-mirrors --geoip
 
 # If using other Arch distros
 sudo pacman -S reflector
 sudo reflector -f 30 -l 30 --number 10 -p http -p https --verbose --save /etc/pacman.d/mirrorlist
-
 
 echo -e "${YELLOW}Installing base dev tools...${NC}"
 
@@ -24,9 +23,3 @@ cd $HOME && rm -rf paru-bin
 echo -e "${YELLOW}Updating system...${NC}"
 
 paru
-
-echo -e "${YELLOW}Setting up Docker...${NC}"
-
-sudo usermod -aG docker $USER
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
