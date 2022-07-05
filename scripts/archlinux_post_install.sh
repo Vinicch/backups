8ef14bc33f3ed5b86683b878d61228a9b2f4b304
Vinicius Leme Alves <52262918+Vinicch@users.noreply.github.com>
2022-07-05 12:24:48 -0300

    Update archlinux_post_install.sh

--- scripts/archlinux_post_install.sh
+++ scripts/archlinux_post_install.sh
@@ -1,14 +1,16 @@
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
+echo "Are you using Manjaro? [y/n]"
+read -r IS_MANJARO
+
 echo -e "${YELLOW}Updating mirrors...${NC}"
-
-# If using Manjaro
-#sudo pacman-mirrors -c all
-
-# If using other Arch distros
-sudo pacman -S reflector
-sudo reflector -f 30 -l 30 --number 10 -p http -p https --verbose --save /etc/pacman.d/mirrorlist
+if [ $IS_MANJARO = y ]; then
+    sudo pacman-mirrors -c all
+else
+    sudo pacman -S reflector
+    sudo reflector -f 30 -l 30 --number 10 -p http -p https --verbose --save /etc/pacman.d/mirrorlist
+fi
 
 echo -e "${YELLOW}Installing base dev tools...${NC}"
 
