58eef63e0bad0ae371aa1d609fe5959010d281cc
Vinicius Leme Alves <52262918+Vinicch@users.noreply.github.com>
2022-07-07 23:24:08 -0300

    Update and rename vscode_extensions.sh to plugins.sh

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -1,3 +1,12 @@
+NC='\033[0m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+
+echo -e "${YELLOW}Installing vim-plug...${NC}"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo -e "${YELLOW}Installing VSCode extensions...${NC}"
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
@@ -12,3 +21,5 @@ code --install-extension tamasfe.even-better-toml
 code --install-extension vadimcn.vscode-lldb
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension zhuangtongfa.material-theme
+
+echo -e "${GREEN}All done!${NC}"
